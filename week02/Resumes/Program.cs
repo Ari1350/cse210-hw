0223b91f3327881cfb68fc651b39031177214791
Ari1350 <ariasperaltag@gmail.com>
2025-01-16 01:21:45 -0400

    camb

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,19 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Software Development";
+        job1._jobTitle = "Software Developer";
         job1._company = "Microsoft";
         job1._startYear = 2005;
         job1._endYear = 2020;
 
         Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
+        job2._jobTitle = "Programmer";
+        job2._company = "Amazon";
         job2._startYear = 2020;
         job2._endYear = 2025;
-
-        job1.Display();
-        job2.Display();
 
         Resume myResume = new Resume();
         
